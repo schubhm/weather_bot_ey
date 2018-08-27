06c567a161205d469108b719c06afc1300ce15e8
vikasGhildiyal <41319365+vikasGhildiyal@users.noreply.github.com>
2018-08-27 23:26:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def makeWebhookResult(req):
         "source": "apiai-weather-webhook-sample"
     }
 if __name__ == '__main__':
-    #port = int(os.getenv('PORT', 5000))
-    #app.run(debug=False, port=port, host='0.0.0.0')
-     app.run(debug=True,port=port,host='127.0.0.1')	
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=False, port=port, host='0.0.0.0')
+     #app.run(debug=True,port=port,host='127.0.0.1')	
